4d8b11aa6b67a448f844c0e552bca238e727122d
smdi <smdimran838@gmial.com>
2018-12-13 11:17:04 +0530

    functions

--- functionsex.py
+++ functionsex.py
@@ -62,6 +62,198 @@ s = compint(s1,s2)
 
 
 
+print('''
+
+
+dir for printing all the attributes functions of object
+
+
+''')
+
+
+import math
+import builtins
+print(dir(builtins))
+print(dir(math))
+
+
+
+print('''
+
+
+
+
+scopes
+
+
+
+''')
+
+
+
+x = 99
+
+def fun():
+    global  x
+    x = 100
+
+
+fun()
+print(x)
+
+
+print('''
+
+
+
+scopes
+
+
+
+''')
+
+
+
+
+X = 99
+def func1():
+ global X
+ X = 88
+def func2():
+ global X
+ X = 77
+
+
+print(X)
+func1()
+print(X)
+func2()
+print(X)
+
+
+
+
+print('''
+
+
+test for multiples 
+
+
+''')
+
+
+
+def almul(ch,rang):
+    lst , r ,c = [] ,1 ,0
+    for x in rang :
+        r = x
+        for y in range(ch):
+            if  ch == c : break
+            r *= x
+            c += 1
+        lst.append(r)
+        c=1
+    r=1
+    return lst
+
+
+
+
+print('''
+
+
+squares
+
+
+''')
+sq = almul(2,range(5))
+print(sq)
+
+
+print('''
+
+
+cubes
+
+
+''')
+
+cu  = almul(3,range(5))
+print(cu)
+
+
+
+
+
+print('''
+
+
+avoiding mutable arguments changes
+
+
+''')
+
+
+
+
+print('without trick')
+
+def changer(l):
+    l[0] = 'spam'
+
+
+l = [1,2,3,4]
+print(l)
+changer(l)
+print(l)
+
+
+
+print('applying trick')
+
+s = [1,2,3,4]
+print(s)
+changer(s[:])
+print(s)
+
+
+
+
+
+print('''
+
+
+return can send multiple values in form of tuple
+
+
+''')
+
+
+
+def multiple(x,y):
+    x = 3
+    y = [3,4]
+    return x,y
+
+
+x = 1
+L = [ 1, 2]
+x,L = multiple(x,L)
+
+print(x,L)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
